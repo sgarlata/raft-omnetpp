e2192eeecac4f17ad9653d642d337163ac1e3c81
StefanoChiodini <79773853+StefanoChiodini@users.noreply.github.com>
2022-03-24 19:08:04 +0100

    Add files via upload
    Prima Versione progetto single client che comunica con single server

---
+++ SingleClientServer/out/clang-release/ClientSingle.o.d
@@ -0,0 +1,7 @@
+out/clang-release//ClientSingle.o: ClientSingle.cc \
+  C:\omnetpp-5.7-windows-x86_64\omnetpp-5.7\include\omnetpp.h \
+  CustomMessage_m.h
+
+C:\omnetpp-5.7-windows-x86_64\omnetpp-5.7\include\omnetpp.h:
+
+CustomMessage_m.h:
---
+++ SingleClientServer/out/clang-release/CustomMessage_m.o.d
@@ -0,0 +1,7 @@
+out/clang-release//CustomMessage_m.o: CustomMessage_m.cc \
+  CustomMessage_m.h \
+  C:\omnetpp-5.7-windows-x86_64\omnetpp-5.7\include\omnetpp.h
+
+CustomMessage_m.h:
+
+C:\omnetpp-5.7-windows-x86_64\omnetpp-5.7\include\omnetpp.h:
