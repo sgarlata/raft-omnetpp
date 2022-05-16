5192847fda162b80d7113f2c674f36404af4b16c
StefanoChiodini <79773853+StefanoChiodini@users.noreply.github.com>
2022-05-16 17:01:45 +0200

    Delete distributedSystemsProject directory

--- distributedSystemsProject/out/clang-debug/Server.o.d
+++
@@ -1,4 +0,0 @@
-out/clang-debug//Server.o: Server.cc \
-  C:\omnetpp-5.7-windows-x86_64\omnetpp-5.7\include\omnetpp.h
-
-C:\omnetpp-5.7-windows-x86_64\omnetpp-5.7\include\omnetpp.h:
--- distributedSystemsProject/out/clang-release/Ping_m.o.d
+++
@@ -1,6 +0,0 @@
-out/clang-release//Ping_m.o: Ping_m.cc Ping_m.h \
-  C:\omnetpp-5.7-windows-x86_64\omnetpp-5.7\include\omnetpp.h
-
-Ping_m.h:
-
-C:\omnetpp-5.7-windows-x86_64\omnetpp-5.7\include\omnetpp.h:
