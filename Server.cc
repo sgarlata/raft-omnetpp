dfebc12ff3736d45bf0b2ea640d199c98c91e9f6
Antonio Sgarlata <antonio.sgarlata11@gmail.com>
2022-06-01 13:46:27 +0200

    Addition to Server

--- Server.cc
+++ Server.cc
@@ -7,16 +7,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <omnetpp.h>
+#include "Ping_m.h"
 #include <algorithm>
 #include <list>
 #include <random>
 #include <sstream>
-//message list
-#include "Ping_m.h"
-#include "LeaderElection_m.h"
-#include "HeartBeats_m.h"
-#include "VoteReply_m.h"
-#include "VoteRequest_m.h"
+#include <chrono>
 
 using namespace omnetpp;
 
@@ -24,15 +20,17 @@ class Server: public cSimpleModule{
     //this is the class definition that is separated from implementation
     //sto estendendo la classe c simple module da cui eredito tutti i metodi, tra cui ad esempio il metodo get name
     private:
-    cMessage *initPingMessage;
-    cMessage *electionTimeoutExpired;//this will be a self message
+    enum stateEnum {FOLLOWER, LEADER, NULLPT, CANDIDATE};
+    stateEnum serverState;// Current state (Follower, Leader or Candidate)
+    int serverNumber; // Number of servers
+    cMessage *leaderElection;
+    int currentTerm; // Latest term server has seen (initialized to 0 on first boot)
+    int votedFor; // ID of the candidate that received vote in current term (or null if none)
+    int leader; // ID of the leader
+    int commitIndex; // index of highest log entry known to be committed
+    std::vector<int> nextIndex;
+    std::vector<int> matchIndex;
 
-
-    enum stateEnum {FOLLOWER, LEADER, NULLPT, UNDERELECTION};
-    int serverStatus;// this variable contains each server status that could be follower, leader, under election or not reachable.
-    int serverNumber;
-    int temp;
-    protected:
 
 //FOR TIMER IMPLEMENTATION SEE TXC8.CC FROM CUGOLA EXAMPLE, E'LA TIMEOUT EVENT
 //GUARDARE SEMPRE ESEMPIO TXC8.CC E SIMULATION PER CAPIRE COME MANDARE PIU' MESSAGGI
@@ -54,6 +52,18 @@ Define_Module(Server);
 //invoked at simulation starting time
 void Server::initialize(){
     serverNumber = getParentModule()->par("numServer");
+
+    currentTerm = 0; // or 1
+    votedFor = -1;
+    state = FOLLOWER;
+    leader = -1;
+    commitIndex = 0;
+    for (int i = 0; i < serverNumber; ++i) {
+        nextIndex.push_back(1);
+        matchIndex.push_back(0);
+    }
+
+
     //#######this cycle send a message from one server to all other servers linked to it###########################################
 
     int counter_printed = 1;
@@ -67,69 +77,30 @@ void Server::initialize(){
         if (h != this->getIndex() && name == ex) {
 
             EV << std::to_string(counter_printed) + ". " + (gate)->getPathEndGate()->getOwnerModule()->getName() + "[" + std::to_string((gate)->getPathEndGate()->getOwnerModule()->getIndex()) + "]\n";
-            LeaderElection *leaderElection = new LeaderElection("election");
-            leaderElection->setLeaderId(100);
+            leaderElection = new cMessage("election");
             send(leaderElection, gate->getName(), gate->getIndex());
             counter_printed++;
         }
     }
+    EV << "\nRichiesta di election mandata a tutti gli altri server!\n";
+
+
     //#####################################################################################################
-
-
-    //here i send the first ping message
-    initPingMessage = new cMessage("initPingMessage");
-    double randomStartingDelay = uniform(0, 0.05);
-    scheduleAt(simTime() + randomStartingDelay, initPingMessage);
 }
 //here i redefine handleMessage method
 //invoked every time a message enters in the node
 void Server::handleMessage(cMessage *msg){
     Ping *ping = dynamic_cast<Ping*>(msg);
-    LeaderElection *leaderElection = dynamic_cast<LeaderElection*>(msg);
 
-    if(msg == initPingMessage){
-        //#############################################################################################
-
-        //this cycle send a message to only one server linked to client
-
-        for (cModule::GateIterator i(this); !i.end(); i++) {
-            cGate *gate = *i;
-            int h = (gate)->getPathEndGate()->getOwnerModule()->getIndex();
-            const char *name = (gate)->getPathEndGate()->getOwnerModule()->getName();
-            std::string ex = "client";
-            //this is the first ping message, i have to sent it to all clients; is the server leader that sent the first
-            //ping to all this client
-            if (name == ex) {
-                EV << "Sending to server[" + std::to_string((gate)->getPathEndGate()->getOwnerModule()->getIndex()) + "] a ping message...\n";
-                Ping *ping = new Ping("ping");
-                ping->setExecIndex(this->getIndex());//this is the index of server, in this case the leader
-                ping->setClientIndex((gate)->getPathEndGate()->getOwnerModule()->getIndex());
-                send(ping,gate->getName(),gate->getIndex());
-
-            }
-        }
-        bubble("SEND PING TO CLIENT");
-        EV<<"HI IM THE FIRST SERVER PING";
-
-    //#####################################################################################################
-    }
-
-    else if (ping != nullptr){
+    if (ping != nullptr){
                 EV<<"SONO NEL PING";
                 //rimando il ping al client
                 bubble("message from client received");
                 int clientId = ping->getClientIndex();
 
-
-                //ping->setExecIndex((gate)->getPathEndGate()->getOwnerModule()->getIndex());
-                //send(ping,gate->getName(),gate->getIndex());
-
-
-                Ping *ping = new Ping("pong");
-                ping->setExecIndex(this->getIndex());
-
-                ping->setClientIndex(clientId);
-
+                Ping *pong = new Ping("pong");
+                pong->setClientIndex(clientId);
+                pong->setExecIndex(this->getIndex());
                 for (cModule::GateIterator i(this); !i.end(); i++) {
                     cGate *gate = *i;
                     int h = (gate)->getPathEndGate()->getOwnerModule()->getIndex();
@@ -138,27 +109,28 @@ void Server::handleMessage(cMessage *msg){
                     // To send the msg only to the client chosen, not the exec with same index
                     if (h == ping->getClientIndex() && name == cl) {
                         EV << "\n##################### \n";
-                        EV << "... answering to the client[" + std::to_string(ping->getClientIndex()) + "]";
+                        EV << "... answering to the client[" + std::to_string(pong->getClientIndex()) + "]";
                         EV << "\n##################### \n";
-                        send(ping, gate->getName(), gate->getIndex());
+                        send(pong, gate->getName(), gate->getIndex());
                     }
                 }
             }
-
-    else if(leaderElection != nullptr){
+/*
+    //else if(msg == leaderElection){
             bubble("leader election start");
             for (cModule::GateIterator i(this); !i.end(); i++) {
                 cGate *gate = *i;
-                int h = (gate)->getPathEndGate()->getOwnerModule()->getIndex();
+                int h = (gate)->getPathEndGate()->getOwnerModule()->getId();
                 const char *name = (gate)->getPathEndGate()->getOwnerModule()->getName();
                 std::string ex = "server";
 
                 // to avoid self msg and msg to client
-                if (h != this->getIndex() && name == ex) {
+                if (h != this->getId() && name == ex) {
                     send(msg->dup(), gate->getName(), gate->getIndex());
                 }
             }
-    }
+               EV << "\nMessagio del server mandato a tutti i server!\n";
+    //}*/
 
 
 }
