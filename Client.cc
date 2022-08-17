2ea56ae010e4fa00cd518d8a078d20c5db0e844f
ManfrediComella <manfredi.comella@mail.polimi.it>
2022-08-17 12:39:11 +0200

    Fized client pong bug; added some features to manage logs

--- Client.cc
+++ Client.cc
@@ -16,24 +16,26 @@
 
 using namespace omnetpp;
 
-class Client: public cSimpleModule{
+class Client: public cSimpleModule {
     private:
-        bool iAmDead;//this is useful to shut down a client
+        bool iAmDead; //this is useful to shut down a client
         int numberToSend; // this is the number that the client send to the leader, we register this number into the log
 
-        cMessage *failureMsg;//this message is useful to shut down a client
-        cMessage *recoveryMsg;//this message is useful to revive a client
-        cMessage *sendToLog;//this is a message only for leader server, this message contains the number for log
+        cMessage *failureMsg; //this message is useful to shut down a client
+        cMessage *recoveryMsg; //this message is useful to revive a client
+        cMessage *sendToLog; //this is a message only for leader server, this message contains the number for log
+
+        cGate *currLeaderOutputGate;
     protected:
         virtual void initialize() override;
         virtual void handleMessage(cMessage *msg) override;
         virtual void finish() override;
-        virtual LogMessage* generateMessageForLog(int number, char operations);//this method is useful to generate a message that a client have to send to the log in the leader server
+        virtual void sendLogMessage(char operation, char varName, int value); //this method is useful to generate a message that a client have to send to the log in the leader server
 };
 
 Define_Module(Client);
 
-void Client::initialize(){
+void Client::initialize() {
 
     WATCH(iAmDead);
     iAmDead = false;
@@ -46,7 +48,10 @@ void Client::initialize(){
     if (deadProbability < realProb) {
         double randomDelay = uniform(1, maxDeathStart);
         failureMsg = new cMessage("failureMsg");
-        EV << "Here is client[" + std::to_string(this->getIndex()) + "]: I will be dead in " + std::to_string(randomDelay) + " seconds...\n";
+        EV
+                  << "Here is client[" + std::to_string(this->getIndex())
+                          + "]: I will be dead in "
+                          + std::to_string(randomDelay) + " seconds...\n";
         scheduleAt(simTime() + randomDelay, failureMsg);
     }
 //##########################HERE I CHECK IF ALL SERVERS ARE LINKED TO CLIENTS#########################################################
@@ -56,11 +61,17 @@ void Client::initialize(){
     for (cModule::GateIterator i(this); !i.end(); i++) {
         cGate *gate = *i;
         int h = (gate)->getPathEndGate()->getOwnerModule()->getId(); //it returns the id of the client or server
-        const char *name = (gate)->getPathEndGate()->getOwnerModule()->getName(); //it returns a string name as "client" or "server"
+        const char *name =
+                (gate)->getPathEndGate()->getOwnerModule()->getName(); //it returns a string name as "client" or "server"
 
         if (h != this->getId()) {
-            EV << std::to_string(counter_printed) + ". " + (gate)->getPathEndGate()->getOwnerModule()->getName() + "["
-            + std::to_string((gate)->getPathEndGate()->getOwnerModule()->getIndex()) + "]\n";
+            EV
+                      << std::to_string(counter_printed) + ". "
+                              + (gate)->getPathEndGate()->getOwnerModule()->getName()
+                              + "["
+                              + std::to_string(
+                                      (gate)->getPathEndGate()->getOwnerModule()->getIndex())
+                              + "]\n";
             counter_printed++;
         }
     }
@@ -73,78 +84,70 @@ void Client::initialize(){
 
 //the client send only a number to the leader server that insert this number into the log;
 //than the client receive a message from the leader that confirms this operation.
-void Client::handleMessage(cMessage *msg){
-    Ping *pong = dynamic_cast<Ping*>(msg);
+void Client::handleMessage(cMessage *msg) {
+    Ping *ping = dynamic_cast<Ping*>(msg);
     // ############################################### RECOVERY BEHAVIOUR ###############################################
 
-       if (msg == failureMsg) {
-           // Change status
-           iAmDead = true;
+    if (msg == failureMsg) {
+        // Change status
+        iAmDead = true;
 
-           // Schedule Recovery Message
-           recoveryMsg = new cMessage("recoveryMsg");
-           double maxDeathDuration = getParentModule()->par("clientsMaxDeathDuration");
-           double randomFailureTime = uniform(5, maxDeathDuration);
-           EV << "\nClient[" + std::to_string(this->getIndex()) + "] is dead for about: [" + std::to_string(randomFailureTime) + "]\n";
-           scheduleAt(simTime() + randomFailureTime, recoveryMsg);
-       }
+        // Schedule Recovery Message
+        recoveryMsg = new cMessage("recoveryMsg");
+        double maxDeathDuration = getParentModule()->par(
+                "clientsMaxDeathDuration");
+        double randomFailureTime = uniform(5, maxDeathDuration);
+        EV
+                  << "\nClient[" + std::to_string(this->getIndex())
+                          + "] is dead for about: ["
+                          + std::to_string(randomFailureTime) + "]\n";
+        scheduleAt(simTime() + randomFailureTime, recoveryMsg);
+    }
 
-       else if (msg == recoveryMsg) {
-           iAmDead = false;
-           EV << "I'm back, let's start working again!\n";
-       }
+    else if (msg == recoveryMsg) {
+        iAmDead = false;
+        EV << "I'm back, let's start working again!\n";
+    }
 
-       // ################################################ NORMAL BEHAVIOUR ################################################
+    // ################################################ NORMAL BEHAVIOUR ################################################
 
-       else if (iAmDead){
-           EV << "At the moment I'm dead so I can't react to this message, sorry \n";
-       }
+    else if (iAmDead) {
+        EV
+                  << "At the moment I'm dead so I can't react to this message, sorry \n";
+    }
 
-       else if (!iAmDead){
-           if (pong != nullptr){
-                    int execIndx = pong->getExecIndex();
+    else if (!iAmDead) {
+        if (ping != nullptr) {
+            int execIndx = ping->getExecIndex();
+            this->currLeaderOutputGate = gateHalf(
+                    ping->getArrivalGate()->getName(), cGate::OUTPUT,
+                    ping->getArrivalGate()->getIndex());
 
-                   bubble("message from server received");
-                   int leaderServerId = pong->getLeaderId();
-                   delete pong;
-
-                   for (cModule::GateIterator i(this); !i.end(); i++) {
-                       cGate *gate = *i;
-                       int h = (gate)->getPathEndGate()->getOwnerModule()->getIndex();
-                       const char *name = (gate)->getPathEndGate()->getOwnerModule()->getName();
-                       std::string ex = "server";
-
-                       // execIndx == h perchè è solo il leader a pingare i/il client e quindi l'index del server salvato in h
-                       // deve essere uguale all'index del server che mi ha mandato il messaggio
-                       if (name == ex && execIndx == h) {
-                           //QUI ANZICHE' IL PONG DOVRANNO ESSERE INVIATI I MESSAGGI PER IL LOG
-                           //CHE SONO STATI CREATI GRAZIE ALLA generateMessageForLog
-                           EV << "Sending to server[" + std::to_string((gate)->getPathEndGate()->getOwnerModule()->getIndex()) + "] a ping message...\n";
-                           Ping *pong = new Ping("ping");
-                           pong->setClientIndex(this->getIndex());
-                           pong->setExecIndex(leaderServerId);
-                           send(pong,gate->getName(),gate->getIndex());
-                       }
-                   }
-                   bubble("here i sent a new ping");
-               }
-       }
-
-
+            bubble("message from server received");
+            int leaderServerId = ping->getLeaderId();
+            delete ping;
+            Ping *pong = new Ping("ping");
+            pong->setClientIndex(this->getIndex());
+            pong->setExecIndex(leaderServerId);
+            send(pong, (currLeaderOutputGate)->getName(),
+                    (currLeaderOutputGate)->getIndex());
+            bubble("here i sent a new ping");
+        }
+    }
 }
 
-LogMessage* Client::generateMessageForLog(int number, char operations) {
-    //DA RIVEDERE, MANCANO ALCUNE COSE
-    int srcClient = this->getIndex();
-    int num = number;
-    char op = operations;
+// It sends a log message, under the assumption that the client already knows a leader
+void Client::sendLogMessage(char operation, char varName, int value) {
 
-    LogMessage *logMessage = new LogMessage ("new entry for log");
-    logMessage->setClientId(srcClient);
-    logMessage->setNum(num);
-    logMessage->setOperation(op);
+    LogMessage *logMessage = new LogMessage("new entry for log");
+    logMessage->setClientId(this->getIndex());
+    logMessage->setOperandValue(value);
+    logMessage->setOperandName(varName);
+    logMessage->setOperation(operation);
 
-    return logMessage;
+    send(logMessage, (currLeaderOutputGate)->getName(),
+            (currLeaderOutputGate)->getIndex());
 }
 
-void Client::finish() {}
+void Client::finish() {
+}
